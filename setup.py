4357030114cd6578e72aab47425e7f555a12ce05
jbilston <jbilston@yelp.com>
2022-08-19 13:01:11 -0600

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mr_job_examples',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='jmasonlee',
+    author_email='',
+    description=''
+)
